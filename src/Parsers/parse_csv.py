616aef0353f2a71e838ed397883427ef736695d6
ImenBoulab Skip to content Navigation Menu <imen.boulabiar@yahoo.com>
2026-03-11 11:08:26 +0000

    Ajout csv

--- src/Parsers/parse_csv.py
+++ src/Parsers/parse_csv.py
@@ -1,5 +1,8 @@
 from src.Common.utils import print_timings
 
+import pandas as pd
+from pathlib import Path
+
 
 @print_timings
 def parse_players_csv(filepath: str, sep: str = ";") -> list:
@@ -8,3 +11,15 @@ def parse_players_csv(filepath: str, sep: str = ";") -> list:
         "vous allez devoir le faire vous-mêmes :("
     )
     return list()
+
+
+
+def read_players_csv():
+    df = pd.read_csv("data/players.csv")
+    return df
+
+
+if __name__ == "__main__":
+    df = read_players_csv()
+    print(df)
+
