8c2ba24a2068cc13cd1222e669a74a0e019d798c
eloragodard <elora.godard@eleve.ensai.fr>
2026-04-02 10:33:30 +0000

    Fin de team

--- src/Model/team.py
+++ src/Model/team.py
@@ -1,8 +1,9 @@
-from typing import Union
-from src.Common.utils import parse_boolean
+from .player import Player
+from .sport import Sport
 
 
 class Team:
+    """Classe modélisant une équipe"""
 
     def __init__(
         self,
@@ -14,7 +15,10 @@ class Team:
         city: str | None,
         state: str | None,
         country: str | None,
-        region: str | None
+        region: str | None,
+        nb_players: int,
+        players: list[Player],
+        sport: Sport
     ) -> None:
 
         self.id = id
@@ -26,3 +30,19 @@ class Team:
         self.state = state
         self.country = country
         self.region = region
+        self.nb_players = nb_players
+        self.players = players
+        self.sport = Sport
+
+    def __str__(self):
+        return "Id de la team : " + self.id + "/n"
+        + "API ID de la team (si existante) : " + self.team_api_id + "/n"
+        + "Nom complet de la team : " + self.full_name + "/n"
+        + "Nom abrégé : " + self.abbreviation + "/n"
+        + "Ville : " + self.city + "/n"
+        + "Etat : " + self.state + "/n"
+        + "Pays : " + self.country + "/n"
+        + "Region du monde : " + self.region + "/n"
+        + "Nombre de joueurs dans l'équipe : " + self.nb_players + "/n"
+        + "Liste des joueurs de l'équipe : " + list[self.players] + "/n"
+        + "Sport de l'équipe : " + self.sport + "/n"
