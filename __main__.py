2c613caded014d2a1d2231430d8369601df682a8
rasmata05 <165924241+rasmata05@users.noreply.github.com>
2026-04-02 12:46:13 +0200

    mayalice

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
 import pandas as pd
-
+from src.Analysis.homemade.TeamFinder import TeamFinder
+from src.Model.sport import Sport
+from src.Model.team import Team
+from src.Parsers.BasketballTeamLoader import BasketballTeamLoader
 from src.Parsers.parse_csv import parse_players_csv
 from src.Analysis.pandas.GoatFinder import find_the_goat_in_df
 from src.Analysis.homemade.GoatFinder import find_the_goat
@@ -19,3 +22,9 @@ if setting == "0":
     the_goat = find_the_goat_in_df(players_df)
 else:
     the_goat = find_the_goat(players)
+
+sport = Sport("basketball", "ballon", 10, "blabla", sport_en_equipe=True)
+A = ["Atlanta Hawks", "Boston Celtics"]
+
+teams = BasketballTeamLoader().load_teams
+print(TeamFinder(teams, A))
--- src/Parsers/BasketballTeamLoader.py
+++ src/Parsers/BasketballTeamLoader.py
@@ -1,20 +1,28 @@
 import pandas as pd
 from ..Model.team import Team
 from ..Model.team import Sport
+
+
 class BasketballTeamLoader:
-    def load_teams():
+    def load_teams(self):
         teams = []
-        df = pd.read_csv("Projet_Traitement-donnees-1A/data/basketball/team.csv", sep=",")
+        df = pd.read_csv(
+            "Projet_Traitement-donnees-1A/data/basketball/team.csv", sep=","
+        )
         for index, row in df.iterrows():
-            team = Team(id=df[index, "id"],
-                        team_api_id=None,
-                        full_name=df[index, "full_name"],
-                        abbreviation=df[index, "abreviation"],
-                        nickname=df[index, "nickname"],
-                        city=df[index, "city"],
-                        state=df[index, "state"],
-                        country=None,
-                        region=None,
-                        sport=Sport("basketball","ballon",10,"blabla",True))
+            team = Team(
+                id=df[index, "id"],
+                team_api_id=None,
+                full_name=df[index, "full_name"],
+                abbreviation=df[index, "abreviation"],
+                nickname=df[index, "nickname"],
+                city=df[index, "city"],
+                state=df[index, "state"],
+                country=None,
+                region=None,
+                nb_players=10,
+                players=list[None],
+                sport=Sport("basketball", "ballon", 10, "blabla", True),
+            )
             teams.append(team)
-        
+            return teams
