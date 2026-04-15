650ac690fe39302c6fe9c3c02c50e6fbc153e6a9
rasmata05 <165924241+rasmata05@users.noreply.github.com>
2026-04-15 12:40:23 +0200

    match loader et footballleagueloader

--- src/Parsers/basketloader.py
+++ src/Parsers/basketloader.py
@@ -3,32 +3,29 @@ from ..Model.team import Team
 from ..Model.sport import Sport
 import pandas as pd
 
+
 class BasketLoader(BaseLoader):
     pass
 
     def __init__(self, filepath):
         super().__init__(filepath)
 
-        self.files = {
-            "players" : "player.csv",
-            "games" : "game.csv",
-            "teams" : "team.csv"
-        }
+        self.files = {"players": "player.csv", "games": "game.csv", "teams": "team.csv"}
 
     def load_data(self):
-        for name,  file in self.files.items():
+        for name, file in self.files.items():
             path = self.filepath + "/" + file
             self.data[name] = pd.read_csv(path)
         return self.data
 
     def teamloader(self):
         data = self.data["teams"]
-        data['team_api_id'] = None
-        data['country'] = "USA"
-        data['region'] = None
-        data['nb_players'] = 0
-        data['players'] = None
-        data['sport'] = Sport('basketball', 'ballon', 10, 'blabla', True)
+        data["team_api_id"] = None
+        data["country"] = "USA"
+        data["region"] = None
+        data["nb_players"] = 0
+        data["players"] = None
+        data["sport"] = Sport("basketball", "ballon", 10, "blabla", True)
 
         """teams = [
             Team(
@@ -48,15 +45,16 @@ class BasketLoader(BaseLoader):
             for _, row in data.iterrows()
         ]"""
 
-        return data[[
-            "id",
-            "team_api_id",
-            "full_name",
-            "abbreviation",
-            "nickname",
-            "city",
-            "state",
-            "country",
-            "region",
-            
-        ]]
+        return data[
+            [
+                "id",
+                "team_api_id",
+                "full_name",
+                "abbreviation",
+                "nickname",
+                "city",
+                "state",
+                "country",
+                "region",
+            ]
+        ]
