de49251ec5491df289db4a8d3e98e8c7dec1d30b
eloragodard <elora.godard@eleve.ensai.fr>
2026-05-07 10:01:22 +0000

    Suppression fichier inutiles

--- src/Analysis/homemade/TeamFinder.py
+++
@@ -1,8 +0,0 @@
-from ...Model.team import Team
-
-def TeamFinder(teams: list[Team], names: list[str]):
-    teams_final = []
-    for i in teams:
-        if i.full_name in names:
-            teams_final.append(i)
-
